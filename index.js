79d469512d5e5f3cf905ba9c99285e82dd86140b
Justin Saborouh <rubikmanjustin@gmail.com>
2022-08-29 10:20:41 -0700

    commit tree 0a6238e4b0232a091bfd8e9cf0b98008ae8ea1e8

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let currentUser = 'Justin';
+let welcomeMessage = "Welcome to Flatbook, ";
+welcomeMessage = welcomeMessage + currentUser;
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+welcomeMessage = welcomeMessage + "!";
+let shortGreeting = "Welcome, " + currentUser[0] + "!";
